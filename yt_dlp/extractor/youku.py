cb635adaf6412d404643e54afd8767f56d3f1103
bashonly <bashonly@bashonly.com>
2023-05-25 21:34:23 -0500

    [extractor/youku] Extract 720p formats

--- yt_dlp/extractor/youku.py
+++ yt_dlp/extractor/youku.py
@@ -1,16 +1,17 @@
+import hashlib
+import json
 import random
 import re
-import string
 import time
 
 from .common import InfoExtractor
 from ..utils import (
     ExtractorError,
-    clean_html,
     get_element_by_class,
     js_to_json,
     str_or_none,
     strip_jsonp,
+    traverse_obj,
 )
 
 
@@ -27,8 +28,51 @@ class YoukuIE(InfoExtractor):
     '''
 
     _TESTS = [{
+        # MD5 is unstable
+        'url': 'http://v.youku.com/v_show/id_XMTc1ODE5Njcy.html',
+        'info_dict': {
+            'id': 'XMTc1ODE5Njcy',
+            'title': '★Smile﹗♡ Git Fresh -Booty Music舞蹈.',
+            'ext': 'mp4',
+            'duration': 74.73,
+            'thumbnail': r're:^https?://.*',
+            'uploader': '。躲猫猫、',
+            'uploader_id': '36017967',
+            'uploader_url': 'http://i.youku.com/u/UMTQ0MDcxODY4',
+            'tags': list,
+        },
+        'skip': '404 Not Found',
+    }, {
         'url': 'http://player.youku.com/player.php/sid/XNDgyMDQ2NTQw/v.swf',
         'only_matching': True,
+    }, {
+        'url': 'http://v.youku.com/v_show/id_XODgxNjg1Mzk2_ev_1.html',
+        'info_dict': {
+            'id': 'XODgxNjg1Mzk2',
+            'ext': 'mp4',
+            'title': '武媚娘传奇 85',
+            'duration': 1999.61,
+            'thumbnail': r're:^https?://.*',
+            'uploader': '疯狂豆花',
+            'uploader_id': '62583473',
+            'uploader_url': 'http://i.youku.com/u/UMjUwMzMzODky',
+            'tags': list,
+        },
+        'skip': '404 Not Found',
+    }, {
+        'url': 'http://v.youku.com/v_show/id_XMTI1OTczNDM5Mg==.html',
+        'info_dict': {
+            'id': 'XMTI1OTczNDM5Mg',
+            'ext': 'mp4',
+            'title': '花千骨 04',
+            'duration': 2363,
+            'thumbnail': r're:^https?://.*',
+            'uploader': '放剧场-花千骨',
+            'uploader_id': '772849359',
+            'uploader_url': 'http://i.youku.com/u/UMzA5MTM5NzQzNg==',
+            'tags': list,
+        },
+        'skip': '404 Not Found',
     }, {
         'url': 'http://v.youku.com/v_show/id_XNjA1NzA2Njgw.html',
         'note': 'Video protected with password',
@@ -42,7 +86,6 @@ class YoukuIE(InfoExtractor):
             'uploader_id': '322014285',
             'uploader_url': 'http://i.youku.com/u/UMTI4ODA1NzE0MA==',
             'tags': list,
-            'skip': '404',
         },
         'params': {
             'videopassword': '100600',
@@ -58,129 +101,211 @@ class YoukuIE(InfoExtractor):
             'thumbnail': r're:^https?://.*',
             'uploader': '明月庄主moon',
             'uploader_id': '38465621',
-            'uploader_url': 'https://www.youku.com/profile/index/?uid=UMTUzODYyNDg0',
+            'uploader_url': 'http://i.youku.com/u/UMTUzODYyNDg0',
             'tags': list,
         },
     }, {
-        'url': 'https://v.youku.com/v_show/id_XNTA2NTA0MjA1Mg==.html',
+        'url': 'http://video.tudou.com/v/XMjIyNzAzMTQ4NA==.html?f=46177805',
         'info_dict': {
-            'id': 'XNTA2NTA0MjA1Mg',
+            'id': 'XMjIyNzAzMTQ4NA',
             'ext': 'mp4',
-            'title': 'Minecraft我的世界：建造超大巨型航空飞机，菜鸟vs高手vs黑客',
-            'duration': 542.13,
+            'title': '卡马乔国足开大脚长传冲吊集锦',
+            'duration': 289,
             'thumbnail': r're:^https?://.*',
-            'uploader': '波哥游戏解说',
-            'uploader_id': '156688084',
-            'uploader_url': 'https://www.youku.com/profile/index/?uid=UNjI2NzUyMzM2',
+            'uploader': '阿卜杜拉之星',
+            'uploader_id': '2382249',
+            'uploader_url': 'http://i.youku.com/u/UOTUyODk5Ng==',
             'tags': list,
         },
+        'skip': 'Tudou domain no longer exists',
     }, {
-        'url': 'https://v.youku.com/v_show/id_XNTE1MzczOTg4MA==.html',
-        'info_dict': {
-            'id': 'XNTE1MzczOTg4MA',
-            'ext': 'mp4',
-            'title': '国产超A特工片',
-            'duration': 362.97,
-            'thumbnail': r're:^https?://.*',
-            'uploader': '陈晓娟说历史',
-            'uploader_id': '1640913339',
-            'uploader_url': 'https://www.youku.com/profile/index/?uid=UNjU2MzY1MzM1Ng==',
-            'tags': list,
-        },
+        'url': 'http://video.tudou.com/v/XMjE4ODI3OTg2MA==.html',
+        'only_matching': True,
     }]
 
-    @staticmethod
-    def get_ysuid():
-        return '%d%s' % (int(time.time()), ''.join(
-            random.choices(string.ascii_letters, k=3)))
+    _CKEY = 'DIl58SLFxFNndSV1GFNnMQVYkx1PP5tKe1siZu/86PR1u/Wh1Ptd+WOZsHHWxysSfAOhNJpdVWsdVJNsfJ8Sxd8WKVvNfAS8aS8fAOzYARzPyPc3JvtnPHjTdKfESTdnuTW6ZPvk2pNDh4uFzotgdMEFkzQ5wZVXl2Pf1/Y6hLK0OnCNxBj3+nb0v72gZ6b0td+WOZsHHWxysSo/0y9D2K42SaB8Y/+aD2K42SaB8Y/+ahU+WOZsHcrxysooUeND'
 
-    def get_format_name(self, fm):
-        _dict = {
-            '3gp': 'h6',
-            '3gphd': 'h5',
-            'flv': 'h4',
-            'flvhd': 'h4',
-            'mp4': 'h3',
-            'mp4hd': 'h3',
-            'mp4hd2': 'h4',
-            'mp4hd3': 'h4',
-            'hd2': 'h2',
-            'hd3': 'h1',
-        }
-        return _dict.get(fm)
+    def _get_cna(self, url, video_id):
+        cna_cookie = self._get_cookies('https://log.mmstat.com').get('cna')
+        if cna_cookie:
+            return cna_cookie.value
 
-    def _real_extract(self, url):
-        video_id = self._match_id(url)
-
-        self._set_cookie('youku.com', '__ysuid', self.get_ysuid())
-        self._set_cookie('youku.com', 'xreferrer', 'http://www.youku.com')
-
-        _, urlh = self._download_webpage_handle(
-            'https://log.mmstat.com/eg.js', video_id, 'Retrieving cna info')
+        self.report_warning('Please consider using cookies to avoid waiting for CNA registration')
+        _, urlh = self._download_webpage_handle('https://log.mmstat.com/eg.js', video_id, 'Retrieving CNA')
         # The etag header is '"foobar"'; let's remove the double quotes
         cna = urlh.headers['etag'][1:-1]
+        time.sleep(12)
 
-        # request basic data
-        basic_data_params = {
+        self._download_webpage(
+            'https://fourier.taobao.com/rp', video_id, note='Registering CNA', query={
+                'ext': '51',
+                'data': f'jm_{cna}',
+                'random': str(random.random()).replace('0.', ''),
+                'href': url,
+                'protocol': 'https:'
+            })
+
+        time.sleep(5)
+        return cna
+
+    def _get_web_api_response(self, url, video_id, cna, client_ts):
+        app_version = '4.1.4'
+        app_key = '24679788'
+
+        data_string = json.dumps({
+            'steal_params': json.dumps({
+                'ccode': '0502',
+                'version': app_version,
+                'client_ts': int(client_ts / 1000),
+                'client_ip': '192.168.1.1',
+                'utid': cna,
+                'ckey': self._CKEY,
+            }),
+            'biz_params': json.dumps({
+                'vid': video_id,
+                'app_ver': app_version,
+            }),
+            'ad_params': json.dumps({
+                'vs': '1.0',
+                'pver': app_version,
+            }),
+        })
+
+        token_cookie = self._get_cookies('https://acs.youku.com/').get('_m_h5_tk')
+        token = token_cookie.value.split('_')[0] if token_cookie else ''
+
+        query = {
+            'api': 'mtop.youku.play.ups.appinfo.get',
+            'v': '1.1',
+            'jsv': '2.6.1',
+            'appKey': app_key,
+            't': client_ts,
+            'sign': hashlib.md5(f'{token}&{client_ts}&{app_key}&{data_string}'.encode('utf-8')).hexdigest(),
+            'type': 'jsonp',
+            'dataType': 'jsonp',
+            'callback': 'mtopjsonp1',
+            'data': data_string,
+        }
+
+        api_response = self._search_json(r'mtopjsonp1\(', self._download_webpage(
+            'https://acs.youku.com/h5/mtop.youku.play.ups.appinfo.get/1.1/',
+            video_id, query=query), 'api_response', video_id)
+
+        for message in api_response.get('ret'):
+            if 'SUCCESS' in message:
+                break
+
+            if 'FAIL_SYS_TOKEN_EMPTY' in message or 'FAIL_SYS_TOKEN_EXOIRED' in message:
+                if not self._get_cookies('https://acs.youku.com/').get('_m_h5_tk'):
+                    raise ExtractorError('Could not get token')
+
+                return self._get_web_api_response(url, video_id, cna, client_ts)
+
+        api_error = traverse_obj(api_response, ('data', 'data', 'error'))
+        if api_error:
+            self.report_warning(f'Youku server reported error {api_error.get("code")}: {api_error.get("note")}')
+
+        return traverse_obj(api_response, ('data', 'data'))
+
+    def _get_mobile_api_response(self, video_id, cna, client_ts):
+        query = {
             'vid': video_id,
-            'ccode': '0524',
-            'client_ip': '192.168.1.1',
+            'ccode': '0501',
+            'client_ip': '0.0.0.0',
+            'app_ver': '1.0.75',
+            'client_ts': client_ts,
             'utid': cna,
-            'client_ts': time.time() / 1000,
+            'ckey': self._CKEY,
         }
 
         video_password = self.get_param('videopassword')
         if video_password:
-            basic_data_params['password'] = video_password
+            query['password'] = video_password
 
         headers = {
-            'Referer': url,
+            'Referer': 'https://m.youku.com',
         }
         headers.update(self.geo_verification_headers())
-        data = self._download_json(
-            'https://ups.youku.com/ups/get.json', video_id,
-            'Downloading JSON metadata',
-            query=basic_data_params, headers=headers)['data']
+        api_response = self._download_json(
+            'https://ups.youku.com/ups/get.json', video_id, 'Downloading JSON metadata', query=query, headers=headers)
 
-        error = data.get('error')
-        if error:
-            error_note = error.get('note')
-            if error_note is not None and '因版权原因无法观看此视频' in error_note:
-                raise ExtractorError(
-                    'Youku said: Sorry, this video is available in China only', expected=True)
-            elif error_note and '该视频被设为私密' in error_note:
-                raise ExtractorError(
-                    'Youku said: Sorry, this video is private', expected=True)
-            else:
-                msg = 'Youku server reported error %i' % error.get('code')
-                if error_note is not None:
-                    msg += ': ' + clean_html(error_note)
-                raise ExtractorError(msg)
+        api_error = api_response.get('e')
+        if api_error.get('code') != 0:
+            raise ExtractorError(
+                f'Youku server reported error {api_error.get("code")}: {api_error.get("note")}')
 
-        # get video title
-        video_data = data['video']
-        title = video_data['title']
+        return api_response.get('data')
 
-        formats = [{
-            'url': stream['m3u8_url'],
-            'format_id': self.get_format_name(stream.get('stream_type')),
-            'ext': 'mp4',
-            'protocol': 'm3u8_native',
-            'filesize': int(stream.get('size')),
-            'width': stream.get('width'),
-            'height': stream.get('height'),
-        } for stream in data['stream'] if stream.get('channel_type') != 'tail']
+    def _extract_video_formats_and_subtitles(self, video_id, api_response):
+        formats, subtitles = [], {}
+        for format in api_response.get('stream') or ():
+            fmts, subs = self._extract_m3u8_formats_and_subtitles(
+                format.get('m3u8_url'), video_id, ext='mp4', fatal=False)
+            for f in fmts:
+                f['height'] = format.get('height')
+                f['width'] = format.get('width')
+
+            formats.extend(fmts)
+            self._merge_subtitles(subs, target=subtitles)
+
+        return formats, subtitles
+
+    def _extract_thumbnails(self, api_response):
+        thumbnails = []
+        for thumbnail_id in ('show_thumburl', 'show_thumburl_huge', 'show_thumburl_big_jpg',
+                             'show_vthumburl_huge', 'show_vthumburl_big_jpg', 'show_vthumburl'):
+            thumbnail_url = traverse_obj(api_response, ('show', thumbnail_id))
+            if thumbnail_url:
+                thumbnails.append({
+                    'id': thumbnail_id,
+                    'url': thumbnail_url,
+                })
+
+        if traverse_obj(api_response, ('video', 'logo')):
+            thumbnails.append({
+                'id': 'logo',
+                'url': traverse_obj(api_response, ('video', 'logo'))
+            })
+
+        return thumbnails
+
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
+        cna = self._get_cna(url, video_id)
+        client_ts = int(time.time() * 1000)
+
+        web_api_response = self._get_web_api_response(url, video_id, cna, client_ts)
+        mobile_api_response = self._get_mobile_api_response(video_id, cna, client_ts)
+
+        formats, subtitles = [], {}
+        web_formats, web_subtitles = self._extract_video_formats_and_subtitles(video_id, web_api_response)
+        mobile_formats, mobile_subtitles = self._extract_video_formats_and_subtitles(video_id, mobile_api_response)
+
+        formats.extend(web_formats)
+        formats.extend(mobile_formats)
+        self._merge_subtitles(web_subtitles, mobile_subtitles, target=subtitles)
+
+        thumbnails = []
+        thumbnails.extend(self._extract_thumbnails(web_api_response))
+        thumbnails.extend(self._extract_thumbnails(mobile_api_response))
 
         return {
             'id': video_id,
-            'title': title,
+            'title': (traverse_obj(web_api_response, ('video', 'title'))
+                      or traverse_obj(mobile_api_response, ('video', 'title'))),
             'formats': formats,
-            'duration': video_data.get('seconds'),
-            'thumbnail': video_data.get('logo'),
-            'uploader': video_data.get('username'),
-            'uploader_id': str_or_none(video_data.get('userid')),
-            'uploader_url': data.get('uploader', {}).get('homepage'),
-            'tags': video_data.get('tags'),
+            'subtitles': subtitles,
+            'thumbnails': thumbnails,
+            'duration': (traverse_obj(web_api_response, ('video', 'seconds'))
+                         or traverse_obj(mobile_api_response, ('video', 'seconds'))),
+            'uploader': (traverse_obj(web_api_response, ('uploader', 'username'))
+                         or traverse_obj(mobile_api_response, ('uploader', 'username'))),
+            'uploader_id': str_or_none(traverse_obj(
+                web_api_response, ('video', 'userid')) or traverse_obj(mobile_api_response, ('video', 'userid'))),
+            'uploader_url': (traverse_obj(web_api_response, ('uploader', 'homepage'))
+                             or traverse_obj(mobile_api_response, ('uploader', 'homepage'))),
+            'tags': (traverse_obj(web_api_response, ('video', 'tags'))
+                     or traverse_obj(mobile_api_response, ('video', 'tags'))),
         }
 
 

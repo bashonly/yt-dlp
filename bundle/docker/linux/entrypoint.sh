3cb4677bf2c64b249262ed08e1f97ecf582c5bf3
bashonly <bashonly@protonmail.com>
2025-08-09 20:14:33 -0500

    3.13
    Authored by: bashonly

--- bundle/docker/linux/entrypoint.sh
+++ bundle/docker/linux/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-python -m venv ~/yt-dlp-build-venv
+python3.13 -m venv ~/yt-dlp-build-venv
 source ~/yt-dlp-build-venv/bin/activate
-python -m devscripts.install_deps -o --include build
-python -m devscripts.install_deps --include secretstorage --include curl-cffi --include pyinstaller
-python -m devscripts.make_lazy_extractors
-python devscripts/update-version.py -c "${channel}" -r "${origin}" "${version}"
-python -m bundle.pyinstaller
+python3.13 -m devscripts.install_deps -o --include build
+python3.13 -m devscripts.install_deps --include secretstorage --include curl-cffi --include pyinstaller
+python3.13 -m devscripts.make_lazy_extractors
+python3.13 devscripts/update-version.py -c "${channel}" -r "${origin}" "${version}"
+python3.13 -m bundle.pyinstaller

490fe39bc9d61c5f9886cf2a29537b040d3a49e5
bashonly <bashonly@protonmail.com>
2026-01-10 00:26:38 -0600

    try things

--- bundle/docker/linux/denotest.sh
+++ bundle/docker/linux/denotest.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 set -exuo pipefail
 
-pipx ensurepath
-pipx install --verbose ".[default,deno]"
+python -m venv /yt-dlp-build-venv
+# shellcheck disable=SC1091
+source /yt-dlp-build-venv/bin/activate
+
+python -m ensurepip --upgrade --default-pip
+python -m pip install -U pip
+
+python -m pip install -U ".[default,deno]"
 
 yt-dlp -v || true
 
-pipx run deno --version
+deno --version || true
+
+python -c "import deno; print(deno.find_deno_bin())"
+
+deno_exe=$(python -c "import deno; print(deno.find_deno_bin())")
+
+chmod +x "${deno_exe}"
+
+"${deno_exe}" --version

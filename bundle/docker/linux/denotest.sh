a065840200d6c36f233a37e1ae19a865cc41ae6b
bashonly <bashonly@protonmail.com>
2026-01-10 04:30:39 -0600

    do the deno even more

--- bundle/docker/linux/denotest.sh
+++ bundle/docker/linux/denotest.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -exuo pipefail
 
+pipx install ".[default,deno]"
+
+yt-dlp -v || true
+
+exit 0
+
+# OLD:
+
+
 function runpy {
     /opt/python/cp313-cp313/bin/python "$@"
 }
@@ -9,6 +18,7 @@ function venvpy {
     python3.13 "$@"
 }
 
+
 runpy -m venv /yt-dlp-build-venv
 # shellcheck disable=SC1091
 source /yt-dlp-build-venv/bin/activate
@@ -16,8 +26,7 @@ source /yt-dlp-build-venv/bin/activate
 venvpy -m ensurepip --upgrade --default-pip
 venvpy -m pip install -U pip
 
-venvpy -m pip install -U ".[default]"
-venvpy -m pip install "deno @ https://github.com/bashonly/deno_pypi/releases/download/v2.6.4/deno-2.6.4.tar.gz"
+venvpy -m pip install -U ".[default,deno]"
 
 yt-dlp -v || true
 

287a4415b25c8d58cdd642b0959c1c3105eb9a97
bashonly <bashonly@protonmail.com>
2026-04-02 14:31:43 -0500

    [devscripts] Refactor `update_ejs`/`utils` to accommodate protobug
    Authored by: bashonly

--- devscripts/utils.py
+++ devscripts/utils.py
@@ -1,8 +1,15 @@
+from __future__ import annotations
+
 import argparse
+import collections.abc
+import contextlib
 import datetime as dt
 import functools
+import io
 import re
 import subprocess
+import urllib.request
+import zipfile
 
 
 def read_file(fname):
@@ -64,3 +71,89 @@ def run_process(*args, **kwargs):
         kwargs.setdefault('encoding', 'utf-8')
         kwargs.setdefault('errors', 'replace')
     return subprocess.run(args, **kwargs)
+
+
+def request(url: str):
+    return contextlib.closing(urllib.request.urlopen(url))
+
+
+def list_wheel_contents(
+        wheel_data: bytes,
+        package_dir: str,
+        suffix: str | None = None,
+        folders: bool = True,
+        files: bool = True,
+        excludes: list[str] | None = None,
+) -> str:
+    assert folders or files, 'at least one of "folders" or "files" must be True'
+
+    if excludes is None:
+        excludes = []
+
+    with zipfile.ZipFile(io.BytesIO(wheel_data)) as zipf:
+        path_gen = (zinfo.filename for zinfo in zipf.infolist())
+
+    filtered = filter(lambda path: path.startswith(f'{package_dir}/') and path not in excludes, path_gen)
+    if suffix:
+        filtered = filter(lambda path: path.endswith(f'.{suffix}'), filtered)
+
+    files_list = list(filtered)
+    if not folders:
+        return ' '.join(files_list)
+
+    folders_list = list(dict.fromkeys(path.rpartition('/')[0] for path in files_list))
+    if not files:
+        return ' '.join(folders_list)
+
+    return ' '.join(folders_list + files_list)
+
+
+def requirements_needs_update(
+    lines: collections.abc.Iterable[str],
+    package: str,
+    version: str,
+):
+    identifier = f'{package}=='
+    for line in lines:
+        if line.startswith(identifier):
+            return not line.removeprefix(identifier).startswith(version)
+
+    return False
+
+
+def requirements_update(
+    lines: collections.abc.Iterable[str],
+    package: str,
+    new_version: str,
+    new_hashes: list[str],
+):
+    first_comment = True
+    current = []
+    for line in lines:
+        if not line.endswith('\n'):
+            line += '\n'
+
+        if first_comment:
+            comment_line = line.strip()
+            if comment_line.startswith('#'):
+                yield line
+                continue
+
+            first_comment = False
+            yield f'# It was later updated using devscripts/update_{package.removeprefix("yt-dlp-")}.py\n'
+
+        current.append(line)
+        if line.endswith('\\\n'):
+            # continue logical line
+            continue
+
+        if not current[0].startswith(f'{package}=='):
+            yield from current
+
+        else:
+            yield f'{package}=={new_version} \\\n'
+            for digest in new_hashes[:-1]:
+                yield f'    --hash={digest} \\\n'
+            yield f'    --hash={new_hashes[-1]}\n'
+
+        current.clear()

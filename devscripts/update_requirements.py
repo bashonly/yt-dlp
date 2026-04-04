f18be008122c56a7dc4c222c7a17bc00e3636187
bashonly <bashonly@protonmail.com>
2026-04-03 22:19:54 -0500

    [devscripts] `update_requirements`: Migrate to `uv export`
    Authored by: bashonly

--- devscripts/update_requirements.py
+++ devscripts/update_requirements.py
@@ -10,11 +10,9 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import collections.abc
 import contextlib
 import dataclasses
-import itertools
 import json
 import pathlib
 import re
-import typing
 import urllib.request
 
 from devscripts.tomlparse import parse_toml
@@ -27,12 +25,6 @@ LOCKFILE_PATH = BASE_PATH / 'uv.lock'
 REQUIREMENTS_PATH = BASE_PATH / 'bundle/requirements'
 OUTPUT_TMPL = 'requirements-{}.txt'
 CUSTOM_COMPILE_COMMAND = 'python -m devscripts.update_requirements'
-
-LINUX_GNU_PYTHON_VERSION = '3.13'
-LINUX_MUSL_PYTHON_VERISON = '3.14'
-WINDOWS_INTEL_PYTHON_VERSION = '3.10'
-WINDOWS_ARM64_PYTHON_VERSION = '3.13'
-MACOS_PYTHON_VERSION = '3.14'
 
 EXTRAS_TABLE = 'project.optional-dependencies'
 GROUPS_TABLE = 'dependency-groups'
@@ -47,113 +39,51 @@ LOCK_EXTRAS = {
 
 @dataclasses.dataclass
 class Target:
-    platform: str
-    version: str
     extras: list[str] = dataclasses.field(default_factory=list)
     groups: list[str] = dataclasses.field(default_factory=list)
-    compile_args: list[str] = dataclasses.field(default_factory=list)
+    prune_packages: list[str] = dataclasses.field(default_factory=list)
+    omit_packages: list[str] = dataclasses.field(default_factory=list)
 
+
+LINUX_TARGET = Target(
+    extras=['default', 'curl-cffi', 'secretstorage'],
+    groups=['pyinstaller'],
+)
+WIN64_TARGET = Target(
+    extras=['default', 'curl-cffi'],
+)
 
 INSTALL_DEPS_TARGETS = {
-    'linux-x86_64': Target(
-        platform='x86_64-manylinux2014',
-        version=LINUX_GNU_PYTHON_VERSION,
-        extras=['default', 'curl-cffi', 'secretstorage'],
-        groups=['pyinstaller'],
-    ),
-    'linux-aarch64': Target(
-        platform='aarch64-manylinux2014',
-        version=LINUX_GNU_PYTHON_VERSION,
-        extras=['default', 'curl-cffi', 'secretstorage'],
-        groups=['pyinstaller'],
-    ),
-    'linux-armv7l': Target(
-        platform='linux',
-        version=LINUX_GNU_PYTHON_VERSION,
-        extras=['default', 'curl-cffi', 'secretstorage'],
-        groups=['pyinstaller'],
-    ),
-    'musllinux-x86_64': Target(
-        platform='x86_64-unknown-linux-musl',
-        version=LINUX_MUSL_PYTHON_VERISON,
-        extras=['default', 'curl-cffi', 'secretstorage'],
-        groups=['pyinstaller'],
-    ),
-    'musllinux-aarch64': Target(
-        platform='aarch64-unknown-linux-musl',
-        version=LINUX_MUSL_PYTHON_VERISON,
-        extras=['default', 'curl-cffi', 'secretstorage'],
-        groups=['pyinstaller'],
-    ),
-    'win-x64': Target(
-        platform='x86_64-pc-windows-msvc',
-        version=WINDOWS_INTEL_PYTHON_VERSION,
-        extras=['default', 'curl-cffi'],
-    ),
-    'win-x86': Target(
-        platform='i686-pc-windows-msvc',
-        version=WINDOWS_INTEL_PYTHON_VERSION,
-        extras=['default'],
-    ),
-    'win-arm64': Target(
-        platform='aarch64-pc-windows-msvc',
-        version=WINDOWS_ARM64_PYTHON_VERSION,
-        extras=['default', 'curl-cffi'],
-    ),
+    'linux-x86_64': LINUX_TARGET,
+    'linux-aarch64': LINUX_TARGET,
+    'linux-armv7l': LINUX_TARGET,
+    'musllinux-x86_64': LINUX_TARGET,
+    'musllinux-aarch64': LINUX_TARGET,
+    'win-x64': WIN64_TARGET,
+    'win-arm64': WIN64_TARGET,
+    'win-x86': Target(extras=['default']),
     'macos': Target(
-        platform='macos',
-        version=MACOS_PYTHON_VERSION,
         extras=['default', 'curl-cffi'],
         # NB: Resolve delocate and PyInstaller together since they share dependencies
         groups=['delocate', 'pyinstaller'],
         # curl-cffi and cffi don't provide universal2 wheels, so only directly install their deps
-        # NB: uv's --no-emit-package option is equivalent to pip-compile's --unsafe-package option
-        compile_args=['--no-emit-package', 'curl-cffi', '--no-emit-package', 'cffi'],
+        omit_packages=['curl-cffi', 'cffi'],
     ),
     # We fuse our own universal2 wheels for curl-cffi+cffi, so we need a separate requirements file
     'macos-curl_cffi': Target(
-        platform='macos',
-        version=MACOS_PYTHON_VERSION,
         extras=['curl-cffi'],
         # Only need curl-cffi+cffi in this requirements file; their deps are installed directly
-        compile_args=[
-            # XXX: Try to keep this in sync with curl-cffi's and cffi's transitive dependencies
-            f'--no-emit-package={package}' for package in (
-                'certifi',
-                'markdown-it-py',
-                'mdurl',
-                'pycparser',
-                'pygments',
-                'rich',
-            )
-        ],
+        # XXX: Try to keep these in sync with curl-cffi's and cffi's transitive dependencies
+        prune_packages=['rich'],
+        omit_packages=['certifi', 'pycparser'],
     ),
 }
 
 
-@dataclasses.dataclass
-class PyInstallerTarget:
-    platform: str
-    version: str
-    asset_tag: str
-
-
 PYINSTALLER_BUILDS_TARGETS = {
-    'win-x64-pyinstaller': PyInstallerTarget(
-        platform='x86_64-pc-windows-msvc',
-        version=WINDOWS_INTEL_PYTHON_VERSION,
-        asset_tag='win_amd64',
-    ),
-    'win-x86-pyinstaller': PyInstallerTarget(
-        platform='i686-pc-windows-msvc',
-        version=WINDOWS_INTEL_PYTHON_VERSION,
-        asset_tag='win32',
-    ),
-    'win-arm64-pyinstaller': PyInstallerTarget(
-        platform='aarch64-pc-windows-msvc',
-        version=WINDOWS_ARM64_PYTHON_VERSION,
-        asset_tag='win_arm64',
-    ),
+    'win-x64-pyinstaller': 'win_amd64',
+    'win-x86-pyinstaller': 'win32',
+    'win-arm64-pyinstaller': 'win_arm64',
 }
 
 PYINSTALLER_BUILDS_URL = 'https://api.github.com/repos/yt-dlp/Pyinstaller-Builds/releases/latest'
@@ -248,74 +178,44 @@ def parse_dependency(line: str, comp_op: str = '==') -> Dependency:
         markers=markers or None)
 
 
-def verify_against_lockfile(
-    requirements_path: pathlib.Path,
-    lockfile: dict[str, typing.Any],
-) -> None:
-    with requirements_path.open() as f:
-        for line in f:
-            if line.lstrip().startswith(('--hash=', '#')) or not line.strip():
-                continue
-            dep = parse_dependency(line)
-            # Ignore packages pinned to URL
-            if dep.direct_reference:
-                continue
-            # We don't keep pip in any extras/groups
-            if dep.name == 'pip':
-                continue
-            lock_package = next(pkg for pkg in lockfile['package'] if pkg['name'] == dep.name)
-            lv = lock_package['version']
-            assert dep.version == lv, f'version mismatch for {dep.name}: {dep.version} != {lv}'
+def run_uv_export(
+    *,
+    extras: list[str] | None = None,
+    groups: list[str] | None = None,
+    prune_packages: list[str] | None = None,
+    omit_packages: list[str] | None = None,
+    bare: bool = False,
+    output_file: pathlib.Path | None = None,
+) -> str:
+    return run_process(
+        'uv', 'export',
+        '--no-python-downloads',
+        '--quiet',
+        '--no-progress',
+        '--color=never',
+        '--format=requirements.txt',
+        '--frozen',
+        '--refresh',
+        '--no-emit-project',
+        '--no-default-groups',
+        *(f'--extra={extra}' for extra in (extras or [])),
+        *(f'--group={group}' for group in (groups or [])),
+        *(f'--prune={package}' for package in (prune_packages or [])),
+        *(f'--no-emit-package={package}' for package in (omit_packages or [])),
+        *(['--no-annotate', '--no-hashes', '--no-header'] if bare else []),
+        *([f'--output-file={output_file.relative_to(BASE_PATH)}'] if output_file else []),
+    ).stdout
 
 
 def run_pip_compile(
     *args: str,
-    extras: list[str] | None = None,
-    groups: list[str] | None = None,
-    single: str | None = None,
+    input_line: str,
     platform: str | None = None,
     version: str | None = None,
     bare: bool = False,
-    output_file: str | None = None,
+    output_file: pathlib.Path | None = None,
 ) -> str:
-    assert any((single, extras, groups)), 'one of "extras", "groups", or "single" must be passed'
-    assert not single or not (extras or groups), 'only "extras"/"groups" OR "single" can be passed'
-
-    if single:
-        requirements_input = f'{single}\n'
-    else:
-        requirements_input = run_process(
-            sys.executable, '-m', 'devscripts.install_deps',
-            '--omit-default',
-            '--print',
-            *itertools.chain.from_iterable(itertools.product(['--include-extra'], extras or [])),
-            *itertools.chain.from_iterable(itertools.product(['--include-group'], groups or [])),
-        ).stdout
-
-    if bare:
-        # Lock extra
-        pip_compile_args = [
-            *args,
-            '--no-annotate',
-            '--no-header',
-        ]
-    else:
-        # Bundle requirements
-        pip_compile_args = [
-            *args,
-            '--generate-hashes',
-            '--no-strip-markers',
-            f'--custom-compile-command={CUSTOM_COMPILE_COMMAND}',
-        ]
-
-    if platform:
-        pip_compile_args.append(f'--python-platform={platform}')
-    if version:
-        pip_compile_args.append(f'--python-version={version}')
-    if not (platform or version):
-        # Assume universal resolution
-        pip_compile_args.append('--universal')
-
+    pip_compile_args = list(args)
     if output_file:
         pip_compile_args.append(f'--output-file={output_file}')
 
@@ -326,9 +226,14 @@ def run_pip_compile(
         '--no-progress',
         '--color=never',
         '--format=requirements.txt',
+        '--refresh',
+        '--generate-hashes',
+        '--no-strip-markers',
+        f'--custom-compile-command={CUSTOM_COMPILE_COMMAND}',
+        '--universal',
         *pip_compile_args,
         '-',  # Read from stdin
-        input=requirements_input,
+        input=f'{input_line}\n',
     ).stdout
 
 
@@ -351,62 +256,49 @@ def update_requirements(upgrade_only: str | None = None):
     run_process('uv', 'lock', upgrade_arg)
     lockfile = parse_toml(LOCKFILE_PATH.read_text())
 
-    # Begin bundle requirements generation
+    # Generate bundle requirements
     with contextlib.closing(urllib.request.urlopen(PYINSTALLER_BUILDS_URL)) as resp:
         info = json.load(resp)
 
-    for target_suffix, target in PYINSTALLER_BUILDS_TARGETS.items():
-        asset_info = next(asset for asset in info['assets'] if target.asset_tag in asset['name'])
+    for target_suffix, asset_tag in PYINSTALLER_BUILDS_TARGETS.items():
+        asset_info = next(asset for asset in info['assets'] if asset_tag in asset['name'])
         pyinstaller_version = PYINSTALLER_VERSION_RE.match(asset_info['name']).group('version')
         pyinstaller_builds_deps = run_pip_compile(
             '--no-emit-package=pyinstaller',
             upgrade_arg,
-            single=f'pyinstaller=={pyinstaller_version}',
-            platform=target.platform,
-            version=target.version)
+            input_line=f'pyinstaller=={pyinstaller_version}')
         requirements_path = REQUIREMENTS_PATH / OUTPUT_TMPL.format(target_suffix)
         requirements_path.write_text(PYINSTALLER_BUILDS_TMPL.format(
             pyinstaller_builds_deps, asset_info['browser_download_url'], asset_info['digest']))
-        verify_against_lockfile(requirements_path, lockfile)
 
     for target_suffix, target in INSTALL_DEPS_TARGETS.items():
         requirements_path = REQUIREMENTS_PATH / OUTPUT_TMPL.format(target_suffix)
-        run_pip_compile(
-            *target.compile_args,
-            upgrade_arg,
+        run_uv_export(
             extras=target.extras,
             groups=target.groups,
-            platform=target.platform,
-            version=target.version,
+            prune_packages=target.prune_packages,
+            omit_packages=target.omit_packages,
             output_file=requirements_path)
-        verify_against_lockfile(requirements_path, lockfile)
 
     requirements_path = REQUIREMENTS_PATH / OUTPUT_TMPL.format('pypi-build')
-    run_pip_compile(
-        upgrade_arg,
-        groups=['build'],
-        output_file=requirements_path)
-    verify_against_lockfile(requirements_path, lockfile)
+    run_uv_export(groups=['build'], output_file=requirements_path)
 
     requirements_path = REQUIREMENTS_PATH / OUTPUT_TMPL.format('pip')
     run_pip_compile(
         upgrade_arg,
-        single='pip',
+        input_line='pip',
         output_file=requirements_path)
-    verify_against_lockfile(requirements_path, lockfile)
-    # End bundle requirements generation
 
     # Generate locked extras
     for lock_name, extra_name in LOCK_EXTRAS.items():
         lock_extra = extras[lock_name] = []
-        compiled_extra = run_pip_compile(upgrade_arg, extras=[extra_name], bare=True)
+        compiled_extra = run_uv_export(extras=[extra_name], bare=True)
         for line in compiled_extra.splitlines():
             dep = parse_dependency(line)
-            lock_package = next(pkg for pkg in lockfile['package'] if pkg['name'] == dep.name)
-            lv = lock_package['version']
-            assert lv == dep.version, f'version mismatch for {dep.name}: {lv} != {dep.version}'
-            wheels = lock_package['wheels']
-            assert wheels, f'lockfile wheels list is empty for {dep.name} in "{lock_name}"'
+            wheels = next((
+                pkg.get('wheels') for pkg in lockfile['package']
+                if pkg['name'] == dep.name and pkg['version'] == dep.version), None)
+            assert wheels, f'no wheels found for {dep.name} in lockfile'
             # If there are platform-specific wheels, then the best we can do is pin to exact version
             if len(wheels) > 1:
                 lock_extra.append(line)
